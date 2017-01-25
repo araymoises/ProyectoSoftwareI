f3810e054f05be547b9a754a1e90e89da17664c0
araymoises <araymoises55@gmail.com>
2017-01-24 23:32:40 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,16 @@
-var express = require('express');
+var express =  require ('express');
+var apiRouter =  require ('./routes/apiRouter');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var path = require('path');
-var expressValidator = require('express-validator');
 
+var db = mongoose.connect("mongodb://localhost/db");
 
-var app = express();
+var app =  express();
 
-//EJS.
-app.set('view engine', 'ejs');
-
-//Ésto es para usar HTML con datos dinámicos.
-app.set('views', path.join(__dirname, 'resources'));
-
-//Middleware.
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 
-//Ésto es para usar HTML de manera estática.
-  //app.use(express.static(path.join(__dirname, 'public')));
-
-//Utilizando el index.ejs
-app.get('/', function(req, res){
-  res.render('index', {
-    world: 'Mundo'
-  });
+app.listen(3000, function(req){
+  console.log('API running on port 3000.');
 });
 
-//Utilizando el puerto 3000.
-app.listen(3000, function() {
-  console.log('Se ha iniciado el servidor en el puerto 3000.');
-})
+app.use('/api', apiRouter);
